a3c73305f4f73ce35532c2de7339357a9d34ba0d
MinHieu2811 <cuunhatnhat51@gmail.com>
2023-04-30 15:14:14 +0700

    add indivisual listing page

--- app/actions/getListings.ts
+++ app/actions/getListings.ts
@@ -1,14 +1,23 @@
-import prisma from '@/app/libs/prismadb';
+import prisma from "@/app/libs/prismadb";
+import { SafeListing } from "../types";
 
 export default async function getListings() {
   try {
     const listing = await prisma.listing.findMany({
       orderBy: {
-        createdAt: 'desc'
-      }
-    })
+        createdAt: "desc",
+      },
+    });
 
-    return listing
+    const safeListing = listing?.map(
+      (list) =>
+        ({
+          ...list,
+          createdAt: list?.createdAt?.toISOString(),
+        })
+    );
+
+    return safeListing;
   } catch (error: any) {
     throw new Error(error);
   }
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,11 @@
-import { Listing } from "@prisma/client";
+
 import getCurrentUser from "./actions/getCurrentUser";
 import getListings from "./actions/getListings";
 import ClientOnly from "./components/ClientOnly";
 import Container from "./components/Container";
 import EmptyState from "./components/EmptyState";
 import ListingCard from "./components/listings/ListingCard";
+import { SafeListing } from "./types";
 
 export default async function Home() {
   const listing = await getListings();
@@ -21,7 +22,7 @@ export default async function Home() {
     <ClientOnly>
       <Container>
         <div className="pt-24 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8">
-          {listing?.map((listing: Listing) => {
+          {listing?.map((listing) => {
             return (
               <ListingCard
                 data={listing}
